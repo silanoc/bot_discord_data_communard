744f26ce183cfb50967ef87ac792a04897ac3f68
silanoc <silanoc@gmail.com>
2022-07-19 17:17:01 +0200

    coeur fonctionnel

--- bot_data3.py
+++ bot_data3.py
@@ -12,15 +12,13 @@ version : 0.0.1
 
 # Standard
 import os
-import sys
+
 # Pour le bon fonctionnement
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv #gére le token dans un fichier à part
-# pour interroger wikidata
-from SPARQLWrapper import SPARQLWrapper, JSON
 
-import communards
+#import communards
 
 # indique où est le token
 load_dotenv(dotenv_path = "config")
@@ -29,25 +27,22 @@ class DocBot(commands.Bot):
 
     def __init__(self):
         super().__init__(command_prefix = "!")
-        self.add_commands()
-        
+
     async def on_ready(self):
         print(f"{self.user.display_name} est prêt. Il est connecté.")
         
     async def on_message(self,message):
         print(message.content)
-    
-    def add_commands(self):
-        @self.command(name="Ping", pass_context=True)
-        async def Ping(self, ctx):
-            await ctx.channel.send("Pong")
 
-        @self.command(name="fin")
-        async def fin(self, ctx):
+    async def on_message(self, message):
+        if(message.content.startswith("!ping")):
+            await message.channel.send("Pong")
+            
+        if(message.content.startswith("!fin")):
             print("fermeture")
             await self.close()    
         
 if __name__ == "__main__":
     bot = DocBot()
-    bot.add_cog(communards.Cog_Communard_data(bot))
+    #bot.add_cog(communards.Cog_Communard_data(bot))
     bot.run(os.getenv("TOKEN"))
